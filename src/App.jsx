400d2fc33b9b1b26f2dbf0add8d97973cd4301ca
Marvinel Santos <marvinelsantos@gmail.com>
2023-02-26 16:18:49 +0800

    Initialize App with React + Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./layout/Footer";
+import Main from "./layout/Main";
+import NavBar from "./layout/NavBar";
+
+function App() {
+  return (
+    <div>
+      {/* NavBar */}
+      <NavBar />
+
+      {/* Main */}
+      <Main />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
